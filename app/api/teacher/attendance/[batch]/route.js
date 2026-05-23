0692b7d572934784fea9859ba16cba1f159bb007
Sundergupta <sunder1234gupta@gmail.com>
2026-05-23 10:07:43 +0530

    changing file path

--- app/api/teacher/attendance/[batch]/route.js
+++ app/api/teacher/attendance/[batch]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { connectDB } from '../../../../lib/mongoose';
+import { connectDB } from '../../../../../lib/mongoose';
 import mongoose from 'mongoose';
 
 function getCollection() {

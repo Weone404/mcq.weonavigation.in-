97833a06e8bd917549a3542650b4c00a6fe86b2e
Sundergupta <sunder1234gupta@gmail.com>
2026-05-23 09:44:42 +0530

    fix: remove credentials and fix .gitignore

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("60ca90b22fb40d9e")
+/******/ 		__webpack_require__.h = () => ("221358b391447177")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dab0aac59f7a6c26"; }
+/******/ 		__webpack_require__.h = function() { return "43c9fd99739009f2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
